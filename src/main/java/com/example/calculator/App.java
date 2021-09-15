ed3b5cda433373df36ca8b609fd338a062bc6da2
Hallie <hine113@gmail.com>
2021-09-15 15:25:32 -0500

    align and style items

--- src/main/java/com/example/calculator/App.java
+++ src/main/java/com/example/calculator/App.java
@@ -12,7 +12,9 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("calculator-view.fxml"));
 
-        stage.setScene(new Scene(fxmlLoader.load(), 240, 300));
+        Scene scene = new Scene(fxmlLoader.load(), 240, 300);
+        scene.getStylesheets().add(this.getClass().getResource("styles.css").toExternalForm());
+        stage.setScene(scene);
         stage.show();
     }
 
